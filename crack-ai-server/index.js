5f3a2a36eea552175215dca95aa1b10541331bbf
SiamIslamSagor <msiamislam12345@gmail.com>
2024-10-30 04:43:18 +0600

    added gemini ai with a static prompt on /prompt get route to get an text story.

--- crack-ai-server/index.js
+++ crack-ai-server/index.js
@@ -1,7 +1,24 @@
 const express = require("express");
 const app = express();
+require("dotenv").config();
+const { GoogleGenerativeAI } = require("@google/generative-ai");
 
 const PORT = process.env.PORT || 5000;
+
+const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
+
+const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
+
+app.get("/prompt", async (req, res) => {
+  const prompt = "Write a story about a magic backpack.";
+
+  const result = await model.generateContent(prompt);
+  const text = result.response.text();
+  res.send({
+    data: text,
+    status: 200,
+  });
+});
 
 app.get("/", (req, res) => {
   res.send({
