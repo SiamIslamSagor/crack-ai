c65342b2fd9377581c3ab1c53de2fb790fbe2019
SiamIslamSagor <msiamislam12345@gmail.com>
2024-10-30 05:10:10 +0600

    added a form on /prompt get route with besic styles, and maked prompt dynamic

--- crack-ai-server/index.js
+++ crack-ai-server/index.js
@@ -5,12 +5,64 @@ const { GoogleGenerativeAI } = require("@google/generative-ai");
 
 const PORT = process.env.PORT || 5000;
 
+app.use(express.urlencoded({ extended: true }));
+
 const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
 
 const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
 
+const form = `
+
+    <div
+      style="
+        min-height: 95vh;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        width: 100%;
+        background-color: gainsboro;
+      "
+    >
+      <form
+        method="POST"
+        action="/prompt"
+        style="
+          border: 2px solid gray;
+          border-radius: 24px;
+          padding: 2.5rem;
+          font-size: 2rem;
+          background-color: white;
+          display: flex;
+          justify-content: center;
+          align-items: center;
+          gap: 2rem;
+        "
+      >
+        <textarea name="prompt" id="prompt"           autofocus="true" style="font-size: 1.2rem; padding: 0.5rem"></textarea>
+        <button
+          type="submit"
+          style="
+            padding-inline: 1.5rem;
+            padding-block: 0.5rem;
+            border-radius: 1rem;
+            background-color: purple;
+            text-transform: uppercase;
+            font-size: 1.5rem;
+          "
+        >
+          Generate
+        </button>
+      </form>
+    </div>
+
+`;
+
 app.get("/prompt", async (req, res) => {
-  const prompt = "Write a story about a magic backpack.";
+  res.send(form);
+});
+
+app.post("/prompt", async (req, res) => {
+  const { prompt } = req.body;
 
   const result = await model.generateContent(prompt);
   const text = result.response.text();
